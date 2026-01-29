55d2e05f6c173c923d533eb8c897403567817e6e
Md Jubayed Sultan <67291584+mdjubayed@users.noreply.github.com>
2026-01-29 21:26:35 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,3 +20,6 @@ app.post("/download",(req,res)=>{
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>console.log("Server running on "+PORT));
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
